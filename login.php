011e23e502dfc7c3df5cd9fb28dad699742506b3
Enigma <mederick.bernier@hotmail.ca>
2021-04-02 06:17:46 -0400

    added the login page with some css modifications

--- login.php
+++ login.php
@@ -4,10 +4,30 @@ $title = "chevaleresk - Connexion";
 include_once __DIR__.'/views/header.php';
 ?>
 
-<div class="row z-depth-3">
-    <div class="col s12 m12 l12">
-        Login here
-    </div>
+<div class="row center-align">
+    <h5>Connexion à Chevaleresk</h5>
+</div>
+
+<div class="row">
+    <form class="col s12 z-depth-1">
+        <div class="row">
+            <div class="input-field col s6">
+            <i class="material-icons prefix">person</i>
+            <input placeholder="Nom d'utilisateur" id="username" type="text" class="validate" data-length="20">
+            <label for="username">Nom d'utilisateur:</label>
+            </div>
+            <div class="input-field col s6">
+                <i class="material-icons prefix">password</i>
+                <input placeholder="Mot de passe" id="password" type="password" data-length="20" class="validate">
+                <label for="password">Mot de passe:</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <button class="btn btn-large indigo lighten-3 waves-effect waves-light white-text" type="submit" name="connect">Connexion</button>
+            </div>
+        </div>
+    </form>
 </div>
 
 <?php
