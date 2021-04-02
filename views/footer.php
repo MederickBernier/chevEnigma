3cec0821a3b17ac80270e48ba7e417b59a612273
Enigma <mederick.bernier@hotmail.ca>
2021-04-02 19:33:09 -0400

    added script folder and moved the sql script from admin to scripts since it makes mode sense

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,6 @@
 </div>
 <script src="assets/js/materialize.min.js"></script>
+<script src="assets/js/site.js"></script>
 <script>
     $(document).ready(function(){
         $('.sidenav').sidenav();
