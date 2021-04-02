ad0035a222d82ffbc3860a27c88471cac6786593
Enigma <mederick.bernier@hotmail.ca>
2021-04-02 07:17:04 -0400

    added some css and login modifications and starting to work on the database interactions, still working out the layering out and other ideas

---
+++ src/dataAccess.php
@@ -0,0 +1,86 @@
+<?php
+
+class dataAccess{
+    #region Properties
+    /**
+     * Instance Variable
+     *
+     * @var RDBMS instance
+     */
+    protected $instance;
+
+    /**
+     * Host Variable
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * User Variable
+     *
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * Password Variable
+     *
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Database Variable
+     *
+     * @var string
+     */
+    protected $database;
+    #endregion
+
+    #region Constructor
+    public function __construct(string $user, string $host, string $password, string $database, array $datas = null){
+        if(is_null($datas)){
+            !empty($user) ? setUser($user) : setUser();
+            !empty($host) ? setHost($host) : setHost();
+            !empty($password) ? setPassword($password) : setPassword();
+            !empty($database) ? setDatabase($database) : setDatabase();
+        }else{
+            setFromArray($datas);
+        }
+    }
+    #endregion
+
+
+    #region Getters
+    public function getHost(){ return $this->host; }
+    public function getUser(){ return $this->user; }
+    public function getPassword(){ return $this->password; }
+    public function getDatabase(){ return $this->database; }
+    public function getInstance(){ return $this->instance; }
+    #endregion
+
+    #region Setters
+    protected function setInstance($instance = null){ !is_null($instance) ? $this->instance = $instance : null; }
+    protected function setHost($host = null){ !is_null($host) ? $this->host = $host : null; }
+    protected function setUser($user = null){ !is_null($user) ? $this->user = $user : null; }
+    protected function setPassword($password = null){ !is_null($password) ? $this->password = $password : null; }
+    protected function setDatabase($database = null){ !is_null($database) ? $this->databas = $database : null; }
+    protected function setFromArray(array $datas){
+        foreach($datas as $k => $v){
+            $k = ucfirst($k);
+            switch($k){
+                case "Host":
+                    setHost($v);
+                case "User":
+                    setUser($v);
+                case "Password":
+                    setPassword($v);
+                case "Database":
+                    setDatabase($v);
+            }
+        }
+    }
+    #endregion
+
+}
