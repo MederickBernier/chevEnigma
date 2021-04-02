d61385fbb9ac3c3428aa69ba61cbbcef0a053167
Enigma <mederick.bernier@hotmail.ca>
2021-04-02 08:15:22 -0400

    made some modifications to index.php mostly a header on the same level as the image and 3 buttons

--- index.php
+++ index.php
@@ -5,14 +5,18 @@ include_once __DIR__.'/views/header.php';
 ?>
 
 <div class="row z-depth-3">
-    <div class="col s12 m12 l12">
+    <div class="col s12 m12 l6">
         <img id="chevalereskImg" src="assets/img/chevaleresk200.png"/>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus, nisl nec pretium ornare, turpis ligula vestibulum mauris, et dictum sem metus non lectus. Proin tortor enim, tincidunt a ligula sollicitudin, euismod venenatis ante. Nam a elementum ipsum. Integer rhoncus consequat pulvinar. Donec sollicitudin, quam id pulvinar gravida, urna nulla euismod elit, non suscipit dolor sapien vitae sapien. Sed ac vehicula diam. Curabitur nulla sapien, bibendum at posuere a, fermentum nec nibh.</p>
-
-        <p>Suspendisse vitae pulvinar dolor. Donec aliquet euismod eleifend. Maecenas consequat ligula vitae ligula interdum, sed ultrices lectus porta. Sed dui erat, commodo at sapien vitae, feugiat lobortis dui. Integer facilisis interdum odio, quis convallis magna laoreet sit amet. Integer justo nisi, luctus vitae iaculis ut, lacinia eget nibh. Integer mattis lectus eu ligula convallis, eget vulputate neque vehicula. Donec laoreet felis ut tempus condimentum. Nullam quis ipsum in augue varius placerat et et ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent feugiat imperdiet lacus. Vivamus eu elit a tortor tincidunt consequat ac scelerisque tortor. Aliquam lacinia, purus id vestibulum sollicitudin, mauris diam volutpat sapien, blandit egestas ex enim vitae nulla. Etiam quis accumsan dolor.</p>
-
-        <p>Ut condimentum sapien sapien, eget suscipit est molestie sit amet. Duis hendrerit, eros ut elementum venenatis, justo lorem lacinia lorem, eget mollis mi eros eget mi. Integer blandit vel risus quis mattis. Maecenas et molestie erat. Curabitur fermentum sollicitudin vehicula. Integer eu molestie dolor. Pellentesque molestie odio sed dignissim euismod. Maecenas a dignissim nisi, at tempus tortor. Phasellus tincidunt tempus risus sit amet rutrum. Morbi et auctor diam. Vestibulum sit amet libero sed lacus ultricies tristique. Praesent commodo purus tellus, nec rutrum augue ullamcorper sit amet. Morbi felis nibh, ultrices sit amet semper id, elementum ut elit.</p>
+    </div>
+    <div class="col s12 m12 l6">
+        <h4>Bienvenue sur le site Chevaleresk !</h4>
+    </div>
+    <div class="row">
+        <div class="col s12 m12 l12">
+            <a class="waves-effect waves-light btn-large teal white-text" href="login.php"><i class="material-icons">cloud</i> Connectez vous</a>
+            <a class="waves-effect waves-light btn-large teal white-text" href="register.php"><i class="material-icons">app_registration</i> Enregistrez vous</a>
+            <a class="waves-effect waves-light btn-large teal white-text" href="market.php"><i class="material-icons">store</i> Accédez au marché</a>
+        </div>
     </div>
 </div>
 
