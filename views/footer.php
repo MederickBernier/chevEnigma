1168c1ca4a18d4862af930c5fc3f4cd5a1253085
Enigma <mederick.bernier@hotmail.ca>
2021-04-03 18:32:28 -0400

    removed jquery but now the navbar sidenav isn't working i gotta find why

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,5 @@
 </div>
 <script src="assets/js/materialize.min.js"></script>
-<script src="assets/js/site.js"></script>
-<script>
-    $(document).ready(function(){
-        $('.sidenav').sidenav();
-    });
-</script>
+<script src="assets/js/materializeEventListeners.js"></script>
 </body>
 </html>
