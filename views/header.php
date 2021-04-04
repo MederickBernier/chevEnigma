f1e4f216a5f383abb3d5988562dfebf2c52e94ad
Enigma <mederick.bernier@hotmail.ca>
2021-04-03 21:23:25 -0400

    added jquery library for sorting and removed marketSort.php since the functionality comes with the jquery lib

--- views/header.php
+++ views/header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href='assets/css/site.css'>
     <link rel="stylesheet" href="assets/css/materialize.min.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.widgets.min.js"></script>
     <title><?php echo $title; ?></title>
 </head>
 <body class="brown lighten-5">
