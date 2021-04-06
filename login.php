450e14d2ca83d586bc1b8a5cb4a781e9f9cdf4cc
Enigma <mederick.bernier@hotmail.ca>
2021-04-05 21:55:14 -0400

    added functionality for displaying the post array right now in login will do the same for register soon

--- login.php
+++ login.php
@@ -9,22 +9,22 @@ include_once __DIR__.'/views/header.php';
 </div>
 
 <div class="row">
-    <form class="col s12 z-depth-1">
+    <form class="col s12 z-depth-1" action="src/users/loginUsers.php" method="POST">
         <div class="row">
             <div class="input-field col s6">
             <i class="material-icons prefix">person</i>
-            <input placeholder="Nom d'utilisateur" id="username" type="text" class="validate" data-length="20">
+            <input placeholder="Nom d'utilisateur" id="username" type="text" class="validate" data-length="20" name="username">
             <label for="username">Nom d'utilisateur:</label>
             </div>
             <div class="input-field col s6">
                 <i class="material-icons prefix">password</i>
-                <input placeholder="Mot de passe" id="password" type="password" data-length="20" class="validate">
+                <input placeholder="Mot de passe" id="password" type="password" data-length="20" class="validate" name="password">
                 <label for="password">Mot de passe:</label>
             </div>
         </div>
         <div class="row">
             <div class="col s12">
-                <button class="btn btn-large indigo lighten-3 waves-effect waves-light white-text" type="submit" name="connect">Connexion</button>
+                <button class="btn btn-large indigo lighten-3 waves-effect waves-light white-text" type="submit">Connexion</button>
             </div>
         </div>
     </form>
--- register.php
+++ register.php
@@ -13,7 +13,7 @@ include_once __DIR__.'/views/header.php';
             <div class="row">
                 <div class="col s12 m12 l12">
                     <div class="input-field s12 l12 m12">
-                        <input placeholder="Nom d'utilisateur" id="username" type="text" class="validate" autocomplete="off" data-length="20">
+                        <input placeholder="Nom d'utilisateur" id="username" name="username" type="text" class="validate" autocomplete="off" data-length="20">
                         <label for="username" class="bold">Nom d'utilisateur</label>
                     </div>
                 </div>
@@ -21,7 +21,7 @@ include_once __DIR__.'/views/header.php';
             <div class="row">
                 <div class="col s12 m12 l12">
                     <div class="input-field s12 l12 m12">
-                        <input placeholder="Mot de Passe" id="password" type="password" class="validate" autocomplete="off" data-length="60">
+                        <input placeholder="Mot de Passe" id="password" name="password" type="password" class="validate" autocomplete="off" data-length="60">
                         <label for="password" class="bold">Mot de passe</label>
                     </div>
                 </div>
@@ -29,7 +29,7 @@ include_once __DIR__.'/views/header.php';
             <div class="row">
                 <div class="col s12 m12 l12">
                     <div class="input-field s12 l12 m12">
-                        <input placeholder="Confirmation Mot de Passe" id="confirmPassword" type="password" class="validate" autocomplete="off" data-length="60">
+                        <input placeholder="Confirmation Mot de Passe" id="confirmPassword" name="confirmPassword" type="password" class="validate" autocomplete="off" data-length="60">
                         <label for="confirmPassword" class="bold">Confirmation du Mot de passe</label>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ include_once __DIR__.'/views/header.php';
             <div class="row">
                 <div class="col s12 m12 l12">
                     <div class="input-field s12 l12 m12">
-                        <input placeholder="Nom Complet" id="fullName" type="text" class="validate" autocomplete="off" data-length="70">
+                        <input placeholder="Nom Complet" id="fullName" name="fullName" type="text" class="validate" autocomplete="off" data-length="70">
                         <label for="fullName" class="bold">Nom Complet</label>
                     </div>
                 </div>
@@ -45,7 +45,7 @@ include_once __DIR__.'/views/header.php';
             <div class="row">
                 <div class="col s12 m12 l12">
                     <div class="input-field s12 l12 m12">
-                        <input placeholder="Adresse Courriel" id="email" type="email" class="validate" autocomplete="off" data-length="70">
+                        <input placeholder="Adresse Courriel" id="email" name="email" type="email" class="validate" autocomplete="off" data-length="70">
                         <label for="email" class="bold">Adresse Courriel</label>
                     </div>
                 </div>
