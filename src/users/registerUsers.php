612a67b0cb5e73fa50ae619037496be196def63c
Enigma <mederick.bernier@hotmail.ca>
2021-04-06 06:42:14 -0400

    recreated dataAccess, working on new file structure for backend and added a .htaccess to prevent direct access to src

--- src/users/registerUsers.php
+++ src/users/registerUsers.php
@@ -2,7 +2,7 @@
 session_start();
 
 require_once '../functions.php';
-require_once '../dataAccess.php';
+require_once '/data/users.data.php';
 require_once '../data.php';
 
 if(!refererVerificator('login.php')){
@@ -12,5 +12,3 @@ if(!refererVerificator('login.php')){
 }
 
 //create the dataAccess object
-$dbObject = new DataAccess($dbDatas["user"],$dbDatas["host"],$dbDatas["password"],$dbDatas["database"]);
-
