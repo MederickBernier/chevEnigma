eb6af20470c7d74370f2978f23a14e3f617a3098
Enigma <mederick.bernier@hotmail.ca>
2021-04-06 06:43:52 -0400

    removed htaccess, it seems i'll have to find another solution

--- src/users/registerUsers.php
+++ src/users/registerUsers.php
@@ -5,10 +5,12 @@ require_once '../functions.php';
 require_once '/data/users.data.php';
 require_once '../data.php';
 
-if(!refererVerificator('login.php')){
+/*if(!refererVerificator('login.php')){
     $path = '../../register.php';
     header("Location:".$path);
     exit;
-}
+}*/
 
+
+echo "REGISTER";
 //create the dataAccess object
